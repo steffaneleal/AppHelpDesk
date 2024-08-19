379f9c4ef160959915f7cb4bd26f782ea5aacd2e
Stéffane Leal <92050677+steffaneleal@users.noreply.github.com>
2024-08-19 15:18:44 -0300

    versão 0
    início

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>App Help Desk</title>
+    <link rel="stylesheet" href="header.css">
+</head>
+<body>
+    <header>
+        <div class="header-right">    
+            <img src="assets/logo.png" alt="">
+            <a href="index.php">App Help Desk</a>
+        </div>
+    </header>
+
+    <main>
+        <form action="<?= $_SERVER["PHP_SELF"]?>" method="get">
+            
+        </form>
+    </main>
+</body>
+</html>
