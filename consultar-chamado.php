43bd061e78fcc5557e0a182f4fda9f674ddd70fb
Stéffane Leal <92050677+steffaneleal@users.noreply.github.com>
2024-08-20 15:33:28 -0300

    versão 1
    Criação das telas de início, menu e abertura de chamados

---
+++ consultar-chamado.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>App Help Desk - Consultar Chamado</title>
+    
+    <!-- links CSS -->
+    <link rel="stylesheet" href="css/header.css">
+</head>
+<body>
+    <header id="header">
+        <div class="header-right">    
+            <img src="assets/logo.png" alt="">
+            <a href="index.php"><h2>App Help Desk</h2></a>
+        </div>
+        <a class="header-left" href="index.php">SAIR</a>
+    </header>
+</body>
+</html>
