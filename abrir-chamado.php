659eb5c1a9be72a4482943ee149cd8be2aa465a3
Stéffane Leal <92050677+steffaneleal@users.noreply.github.com>
2024-08-21 14:02:23 -0300

    versão 1
    Criação das telas: login, menu, abrir chamados e consultar chamados

---
+++ abrir-chamado.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>App Help Desk - Abrir Chamado</title>
+
+    <!-- links CSS -->
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/abrir-chamado.css">
+</head>
+<body>
+    <header id="header">
+        <div class="header-right">    
+            <img src="assets/logo.png" alt="">
+            <h2>App Help Desk</h2>
+        </div>
+        <a class="header-left" href="index.php">SAIR</a>
+    </header>
+    <main>
+        <section id="main-form">
+            <div class="form-abertura-chamado">
+                <h2>Abertura de chamado</h2>
+            </div>
+            <form action="<?=$_SERVER["PHP_SELF"]?>" method="get">
+                <label for="titulo">Título</label>
+                <input type="text" name="titulo" id="titulo" placeholder="Título">
+                <label for="categoria">Categoria</label>
+                <select name="categoria" id="categoria">
+                    <option value="criacao_usuario">Criação Usuário</option>
+                    <option value="impressora">Impressora</option>
+                    <option value="hardware">Hardware</option>
+                    <option value="software">Software</option>
+                    <option value="rede">Rede</option>
+                </select>
+                <label for="descricao">Descrição</label>
+                <input type="text" placeholder="Descrição">
+                <div class="form-buttons">
+                    <input class="form-buttons1" type="submit" value="Voltar">
+                    <input class="form-buttons2" type="submit" value="Abrir">
+                </div>
+            </form>
+        </section>
+    </main>
+</body>
+</html>
