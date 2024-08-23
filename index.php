1cb620f9b98ecfa8cae194d104638d39ee32d36c
Stéffane Leal <92050677+steffaneleal@users.noreply.github.com>
2024-08-23 15:05:34 -0300

    versão 3

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>App Help Desk</title>
+
+    <!--Links CSS -->
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    <header id="header">
+        <div class="header-right">    
+            <img src="assets/logo.png" alt="">
+            <h2>App Help Desk</h2>
+        </div>
+    </header>
+
+    <main>
+        <section id="main-form">
+            <div class="form-login">
+                <h2>Login</h2>
+            </div>
+
+            <form action="valida_login.php" method="post">
+                <input class="form-input" type="email" name="email" id="email" placeholder="E-mail"> <!-- placeholder coloca o "label" dentro do input -->
+                <input class="form-input" type="password" name="senha" id="senha" placeholder="Senha">
+                <? 
+                    //para se der erro na autenticação, redirecionar para essa parte
+                    if(isset($_GET['login']) && $_GET['login'] == 'erro'){ //isset() verifica se um determinado índice de um array está setado
+                ?>
+                        <div class="text-danger" style="color: red; font-size:medium;">
+                            Usuário ou senha inválido(s).
+                        </div>
+                <? 
+                    } //fechamento do if do bloco de php anterior
+                ?>
+                <input class="form-button" type="submit" value="Entrar">
+            </form>
+        </section>
+    </main>
+</body>
+</html>
---
+++ valida_login.php
@@ -0,0 +1,39 @@
+<?php 
+    //variável que verifica se a autenticação foi realizada
+    $usuario_autenticado = false; //ela é declarada como false pq vai ser verdadeiro apenas se passar na condição do primeiro if (estar autenticado)
+
+
+    //usuários do sistema
+    $usuarios_app = array(
+        array('email' => 'adm@teste.com.br', 'senha' => '123456'), 
+        array('email' => 'user@teste.com.br', 'senha' => 'abcd')
+    );
+
+    //laço de repetição para identificar se as credenciais se encontram no "banco"
+    // foreach($usuarios_app as $user){
+    //     echo "Usuário app: " . $user['email'] . ' / ' . $user['senha'];
+    //     echo "<br>";
+    //     echo "Usuário form: " . $_POST['email'] . ' / ' . $_POST['senha'];
+
+    //     echo "<hr>";
+    // }
+
+    //ver se a autenticação bate
+    foreach($usuarios_app as $user){
+        if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){ //verifica se o e-mail e a senha do $_POST (passado pelo usuário) batem com as informações do $user (nosso "banco" improvisado)
+            $usuario_autenticado = true;
+        }
+    }
+
+    if($usuario_autenticado){
+        echo "Usuário autenticado";
+    }else{
+        header('Location: index.php?login=erro'); //função que retorna uma página, nesse caso a função vai levar de volta para o index pq não foi possível ser autenticado
+    }
+
+    // print_r($_POST);
+    // echo "<br>";
+    // echo $_POST["email"];
+    // echo "<br>";
+    // echo $_POST["senha"];
+?>
