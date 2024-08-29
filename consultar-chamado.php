5ae0754e4c3e9ed4950a2a4df6522fae56af36b5
Stéffane Leal <92050677+steffaneleal@users.noreply.github.com>
2024-08-29 09:00:44 -0300

    versão 3.2
    - Resolvi o problema da mensagem de "Usuário ou senha inválido(s)";
    - Agora o valida_login.php redireciona para as telas.

---
+++ consultar-chamado.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>App Help Desk - Consultar Chamado</title>
+    
+    <!-- links CSS -->
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/consulta-chamado.css">
+</head>
+<body>
+    <header id="header">
+        <div class="header-right">    
+            <img src="assets/logo.png" alt="">
+            <a href="index.php"><h2>App Help Desk</h2></a>
+        </div>
+        <a class="header-left" href="index.php">SAIR</a>
+    </header>
+    <main>
+        <section id="main-container">
+            <div class="consulta-header">
+                <h2>Consulta de chamado</h2>
+            </div>
+            <div class="consultas">
+                <a href="home.php"><input class="consultas-button" type="submit" value="Voltar"></a>
+            </div>
+        </section>
+    </main>
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>App Help Desk</title>
+
+    <!-- Links CSS -->
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/menu.css">
+
+</head>
+<body>
+    <header id="header">
+        <div class="header-right">    
+            <img src="assets/logo.png" alt="">
+            <a href="index.php"><h2>App Help Desk</h2></a>
+        </div>
+        <a class="header-left" href="index.php">SAIR</a>
+    </header>
+    <main>
+        <section id="main-menu">
+            <div class="menu-header">
+                <h2>Menu</h2>
+            </div>
+            <div class="menu-cards">
+                <a href="abrir-chamado.php"><img src="assets/formulario_abrir_chamado.png" alt="Abrir Chamado"></a>
+                <a href="consultar-chamado.php"><img src="assets/formulario_consultar_chamado.png" alt="Consultar Chamado"></a>
+            </div>
+        </section>
+    </main>
+</body>
+</html>
